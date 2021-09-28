e35c8bf29add3e1ee0ac1d90e29f0b07b6b13fb5
Fakhrillo <dombit1999@gmail.com>
2021-09-28 11:30:47 +0500

    allowed host

--- fun_shop_v2/settings.py
+++ fun_shop_v2/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fun-shop-v2-env.eba-agbkrnhf.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
