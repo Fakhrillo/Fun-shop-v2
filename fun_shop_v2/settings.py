a6a97effbbe8fb788a574a4b3696d9f023505933
Fakhrullokhon <62103175+Fakhrillo@users.noreply.github.com>
2021-10-09 02:19:49 +0500

    Add files via upload

--- fun_shop_v2/settings.py
+++ fun_shop_v2/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,9 +139,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
